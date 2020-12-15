01c1e10172dd8b0cbb7c12a7132d201ed08da135
Yury <altnightfall@mail.ru>
2020-12-15 22:37:50 +0300

    Logo

--- pages/media.js
+++ pages/media.js
@@ -6,13 +6,13 @@ export default function Media() {
   return (
     <div className="container">
       <Header title="Media | UniDB"/>
-      <div className="container" >
-        <div className = " mt-5 row ">
-          <a href="https://vk.com/activ_fakulteta_cithin" class="btn btn-primary btn-lg active offset-4" role="button" aria-pressed="true">VKontakte</a>
-          <a href="https://www.instagram.com/cithin_muctr/" class="btn btn-danger btn-lg active offset-1" role="button" aria-pressed="true">Instagram</a>
-        </div>
+      <div className="container row mt-5" >
+          <a href="https://vk.com/activ_fakulteta_cithin" className=" btn btn-primary btn-lg active offset-4" role="button" aria-pressed="true">VKontakte</a>
+          <a href="https://www.instagram.com/cithin_muctr/" className="btn btn-danger btn-lg active offset-1" role="button" aria-pressed="true">Instagram</a>
       </div>
-      
+      <div className="row">
+      <img src="./logo.jpg" className="img-fluid rounded mx-auto" alt="Logo"/>
+      </div>
       <Footer/>
     
     </div>
