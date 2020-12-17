222e96b1d3af93f59b8c6d1e9d5262695c44563f
Yury <altnightfall@mail.ru>
2020-12-17 03:05:43 +0300

    ???

--- pages/patent.js
+++ pages/patent.js
@@ -4,15 +4,37 @@ import Header from './header'
 
 export default function Search() {
   return (
-    <div className ="container " style={{height:"100%", width:"100%"}}>
+    <body className ="container " style={{height:"100%", width:"100%"}}>
     <Header title="Main | UniDB"/>
-    <div className="container sm-w-100" style={{width: "50vw"}}>
-
-    // insert  patent
+    <div  className="container sm-w-100" id="patent" style={{width: "50vw"}}>
+      <a href="/search" className="text-left"><h5>Go back</h5></a>
+      <jsonout/>
     </div>
 
     <Footer/>
 
-    </div>
+    </body>
   )
+}
+
+function jsonout(){
+    let jsonData = require('../rudata.json');
+  //console.log(jsonData);
+  let str = ' ';
+  let k=1;
+    /*for (let i = 0; i< jsonData.length; i++  ) {
+      if(jsonData[i].patentNumber =='94039156') k=i;
+    }*/
+    return(
+      <div className="container">
+      <div id="number" className="text-left">{jsonData[k].patentNumber}</div>
+      <div id="country" className="text-left"></div>
+      <div id="mpk" className="text-left"></div>
+      <div id="application" className="text-left"></div>
+      <div id="publicationDate" className="text-left"></div>
+      <div id="authors" className="text-left"></div>
+      <div id="Name" className="text-center"></div>
+      <div id="abstract" className="text-justify"></div>
+    </div>
+    )
 }
